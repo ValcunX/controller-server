b5a47e55a93a35afaec1f396e1fca7d66a63e40b
SaiHemanthBR <sh.bheemreddy@gmail.com>
2021-06-13 15:04:54 +0530

    Added helper scripts

---
+++ scripts/build.sh
@@ -0,0 +1,1 @@
+docker build -t vulcanx/controller-server -f docker/Dockerfile .
---
+++ scripts/start.sh
@@ -0,0 +1,1 @@
+docker run --rm -it --name valcunx_controller-server --env-file ./env.prod -p 5051:80 -p 5050:5050 vulcanx/controller-server
